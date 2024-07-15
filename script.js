adbcefb87cd5a80753c2f9b215f7c047e17bae40
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-16 01:44:33 +0500

    chap 7 done

---
+++ script.js
@@ -0,0 +1,41 @@
+// chapter number 7 Maths expressin
+
+console.log(2-1*8);
+First
+rule in maths first solve in multiply
+1*8=8
+2-8=-6
+
+console.log(1+2*3+7);
+first 
+2*3=6
+1+6=7
+7+7=14
+
+console.log(1+2*(3+7));
+First 
+3+7=10
+2*10=20
+20+1=21
+
+console.log(5*5-30 /10);
+
+first 
+25-30 /10
+30/10=3
+25-3=22
+
+console.log(5*5-3/3+7);
+first 
+25-3/3+7
+25-1+7
+24+7
+31
+
+console.log(2*4*4+2);
+first 
+2*4=8
+8*4=32
+32+2=34
+
+
